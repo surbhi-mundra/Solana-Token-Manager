8ac225c1155c44134eee023d19223f9447ad46ab
Surbhi Mundra <surbhimundra.sm10@gmail.com>
2025-04-01 16:21:43 +0530

    Create utils.ts

---
+++ lib/utils.ts
@@ -0,0 +1,14 @@
+import { type ClassValue, clsx } from "clsx"
+import { twMerge } from "tailwind-merge"
+
+export function cn(...inputs: ClassValue[]) {
+  return twMerge(clsx(inputs))
+}
+
+export function truncateAddress(address: string, startChars = 4, endChars = 4): string {
+  if (!address) return ""
+  if (address.length <= startChars + endChars) return address
+  return `${address.slice(0, startChars)}...${address.slice(-endChars)}`
+}
+
+  
